f928b40ac8ca57718669c60ea29b6612dbc1de02
UrFriendPepe <1755090774@qq.com>
2023-11-13 17:24:49 +0000

    1st

---
+++ Assets/Scripts/PlatformMove.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class PlatformMove : MonoBehaviour
+{
+    GameObject _player;
+    float _pSpeed = 0.002f;//platform speed
+    float _dir;
+    float _timer;
+    bool _moving;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovedByPlayer();
+    }
+
+    void MovedByPlayer()
+    {
+        Vector3 distPlatform = transform.position;
+        Vector3 distPlayer = _player.transform.position;
+        float xDist = distPlatform.x - distPlayer.x;
+        float dir = Vector2.Distance(distPlatform, distPlayer);
+
+        if (_timer <= 0)
+        {
+            _dir = Mathf.Abs(xDist) / (xDist * dir ) ;// does platform go left or right
+        }
+
+        if(_moving)
+        {
+            if(Mathf.Abs(xDist) >= 1)
+            {
+                _timer += Time.deltaTime;
+                this.transform.position += new Vector3(_pSpeed / (_dir / 20), 0, 0);
+            }
+            else if(Mathf.Abs(xDist) <= 1)
+            {
+                _timer = 0;
+                _moving = false;
+            }
+
+            if(_timer >= 0.1f)
+            {
+                _timer = 0;
+                _moving = false;
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            _moving = true;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    Rigidbody2D _rb;
+    float _speed = 10f;
+    float _force = 25f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //XMovement();
+        //YMovement();
+    }
+
+    public void XMovement()
+    {
+        float dirX = Input.GetAxis("Horizontal");
+        _rb.velocity = new Vector2(dirX * _speed, _rb.velocity.y);
+    }
+
+    public void YMovement()
+    {
+        float dirY = Input.GetAxis("Vertical");
+        _rb.velocity = new Vector2(_rb.velocity.x, dirY * _speed);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //if (collision.gameObject.CompareTag("BottomPlatform"))
+        //{
+        //    _rb.AddForce(Vector3.up * _force, ForceMode2D.Impulse);
+        //}
+        //else if (collision.gameObject.CompareTag("TopPlatform"))
+        //{
+        //    _rb.AddForce(Vector3.down * _force, ForceMode2D.Impulse);
+        //}
+        //else if (collision.gameObject.CompareTag("LeftPlatform"))
+        //{
+        //    _rb.AddForce(Vector3.right * _force, ForceMode2D.Impulse);
+        //    print("111111");
+        //}
+        //else if (collision.gameObject.CompareTag("RightPlatform"))
+        //{
+        //    _rb.AddForce(Vector3.left * _force, ForceMode2D.Impulse);
+        //}
+    }
+}
